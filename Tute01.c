0e140f200130bfa3ce6117c320829e159202547b
IT21439032 <87414159+IT21439032@users.noreply.github.com>
2021-07-14 08:06:50 +0000

    Completed

--- Tute01.c
+++ Tute01.c
@@ -4,8 +4,19 @@
 
 #include <stdio.h>
 
-int main() {
-  
-  return 0;
+int main() 
+{
+  int mark1, mark2;
+  float avg;
+
+  printf("Enter mark for subject 1: ");
+  scanf("%d", &mark1);
+  printf("Enter mark for subject 2: ");
+  scanf("%d", &mark2);
+
+  avg = (mark1 + mark2) / 2;
+  printf("Average of marks: %.2f",avg);
+
+   return 0;
 }
 
